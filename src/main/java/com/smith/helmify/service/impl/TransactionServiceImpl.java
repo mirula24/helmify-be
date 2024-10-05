dc1b96882a0d54be54b5c4d370c66221520fe5e1
Muhammad Amirul <mirulid@gmail.com>
2024-10-05 18:21:29 +0700

    resolve conflig

--- src/main/java/com/smith/helmify/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/smith/helmify/service/impl/TransactionServiceImpl.java
@@ -218,7 +218,7 @@ public class TransactionServiceImpl implements TransactionService {
 
         // Charge payment via Midtrans Snap
         MidtransSnapRequestDTO.TransactionDetails transactionDetails = new MidtransSnapRequestDTO.TransactionDetails(
-                "TRANSACTION-" + transaction.getId(),
+                "HLMTRX-" + transaction.getId(),
                 total
         );
         req.setTransaction_details(transactionDetails);
